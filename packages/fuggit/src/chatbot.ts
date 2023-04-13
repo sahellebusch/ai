76ead37232e13ab964731ccc8e8d79b5e55ab481
Sean Hellebusch <sahellebusch@gmail.com>
2023-04-13 15:37:46 -0600

    wooboy

---
+++ packages/fuggit/src/chatbot.ts
@@ -0,0 +1,112 @@
+import * as os from 'os';
+import inquirer from 'inquirer';
+import axios from 'axios';
+
+type Roles = 'system' | 'assistant' | 'user';
+
+type Choice = {
+  message: Message;
+};
+
+type Message = {
+  role: Roles;
+  content: string;
+};
+
+type CompletionType = {
+  choices: Choice[];
+};
+
+type InputResponse = {
+  input: string;
+};
+
+type ChoiceResponse = {
+  choice: boolean;
+};
+
+export class ChatBot {
+  private readonly shell = os.userInfo().shell;
+  private readonly messageHistory: Message[] = [
+    {
+      role: 'system',
+      content: `You are an assistent who helps generate command lines for a the following shell: ${this.shell}. You only respond with code; no text, no explanations, no extra words.`,
+    },
+  ];
+
+  constructor() {
+    if (!process.env.OPENAI_API_KEY) {
+      throw new Error('No OPENAI_API_KEY found.');
+    }
+  }
+
+  async ask(content: string): Promise<string> {
+    try {
+      this.messageHistory.push({
+        role: 'user',
+        content,
+      });
+
+      const edit = await axios<CompletionType>({
+        method: 'post',
+        url: 'https://api.openai.com/v1/chat/completions',
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        data: {
+          model: 'gpt-3.5-turbo',
+          temperature: 0.5,
+          messages: this.messageHistory,
+        },
+      });
+
+      this.messageHistory.push(edit.data.choices[0].message);
+      return this.parseCmd(edit.data.choices[0].message.content);
+    } catch (error) {
+      throw new Error(`Chat error occurred: ${error.data.message}`);
+    }
+  }
+
+  private parseCmd(cmd: string): string {
+    if (!cmd) {
+      return 'Bummer, failed to get a response.';
+    }
+
+    /**
+     * Sometimes the chat will disobey instructions and say something like the following
+     * that will result in a premature regex match
+     * "Here's another way to print environment variables in the `/bin/zsh` shell"
+     */
+    cmd.replace(this.shell, '');
+
+    if (cmd.includes('```')) {
+      // https://regex101.com/r/hWo7PX/1
+      return cmd.match(
+        /```\s*([^]+?.*?[^]+?[^]+?)```|`\s*([^]+?.*?[^]+?[^]+?)`/
+      )[1];
+    }
+
+    return cmd;
+  }
+
+  async promptForInput(message: string): Promise<InputResponse> {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'input',
+        message,
+      },
+    ]);
+  }
+
+  async promptForChoice(message: string): Promise<ChoiceResponse> {
+    return inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'choice',
+        message,
+        default: true,
+      },
+    ]);
+  }
+}
--- packages/fuggit/src/index.ts
+++ packages/fuggit/src/index.ts
@@ -1,1 +1,62 @@
-export * from './lib/fuggit';
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import { green, red, bold } from 'colorette';
+import { ChatBot } from './chatbot.js';
+
+const execAsync = promisify(exec);
+const program = new Command();
+const chatBot = new ChatBot();
+
+async function execCmd(cmd: string): Promise<void> {
+  console.log(bold(green('Attempting command')));
+  const { stdout } = await execAsync(cmd);
+  console.log(green(stdout));
+}
+
+async function run(thePrompt: string): Promise<void> {
+  if (!thePrompt) {
+    console.log(red('Something went wrong, please try agian.'));
+    process.exit(1);
+  }
+
+  const cmd = await chatBot.ask(thePrompt);
+  const { choice } = await chatBot.promptForChoice(
+    `Here's what we got:\n${cmd}\n\nWant to try it?`
+  );
+
+  if (!choice) {
+    const { choice } = await chatBot.promptForChoice(
+      'Want to try generating another command?'
+    );
+    if (!choice) {
+      return;
+    }
+    const { input } = await chatBot.promptForInput('Any more details to add?');
+    await run(input);
+  }
+
+  try {
+    await execCmd(cmd);
+    console.log(bold(green('Success!')));
+    process.exit();
+  } catch (error) {
+    console.log(bold(red("Bummer, it didn't work.")));
+    console.log(red(error.toString()));
+    const { choice } = await chatBot.promptForChoice('Want to try again?');
+
+    if (choice) {
+      await run(thePrompt);
+    }
+    process.exit();
+  }
+}
+
+program.version('0.0.1').action(async () => {
+  const { input } = await chatBot.promptForInput('Whatcha trying to do?');
+  await run(input);
+});
+
+program.parse();
