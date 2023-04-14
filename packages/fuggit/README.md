68cfab38fd1fdb3d15d3395e5aeca2636f3b43e6
Sean Hellebusch <sahellebusch@users.noreply.github.com>
2023-04-14 08:39:47 -0600

    feat(fuggit): add initial fuggit functionality (#1)

--- packages/fuggit/README.md
+++ packages/fuggit/README.md
@@ -1,11 +1,37 @@
-# fuggit
+<p align="center">
+  <img width="300" height="220" src="./fuggit.png">
+</p>
 
-This library was generated with [Nx](https://nx.dev).
+## Purpose 
 
-## Running unit tests
+The purpose of this command line chat utility is to assist users in generating CLI (Command Line Interface) commands by providing suggestions and guidance through an interactive chat interface. The tool aims to simplify the process of coming up with complex CLI commands, especially for users who may not have extensive experience with using command line interfaces.
+
+## Usage 
+
+``` sh
+❯ node dist/packages/fuggit/src/index
+? Whatcha trying to do? I need my terminal langauge
+✔ Requesting help from our overlords
+? Here's what we got:
+echo $LANG
+
+Want to try it? No
+? Want to try generating another command? Yes
+? Any more details to add? I meany my terminal colors for some reason
+✔ Requesting help from our overlords
+? Here's what we got:
+echo -e "\033[0;31m RED \033[0m"
+
+Want to try it? Copy to Clipboard
+```
+
+## Developing
+
+### Running unit tests
 
 Run `nx test fuggit` to execute the unit tests via [Jest](https://jestjs.io).
 
-## Running lint
+### Running lint
 
 Run `nx lint fuggit` to execute the lint via [ESLint](https://eslint.org/).
+
--- packages/fuggit/src/index.ts
+++ packages/fuggit/src/index.ts
@@ -1,1 +1,68 @@
-export * from './lib/fuggit';
+#!/usr/bin/env node
+
+import clipboard from 'clipboardy';
+import { Command } from 'commander';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import { OpenAI } from './openai.js';
+import * as Facilitator from './facilitator.js'
+
+const execAsync = promisify(exec);
+const program = new Command();
+const chatBot = new OpenAI();
+
+async function execCmd(cmd: string): Promise<void> {
+  Facilitator.attemptingCommand()
+  const { stdout } = await execAsync(cmd);
+  console.log(stdout);
+}
+
+async function run(thePrompt: string): Promise<void> {
+  if (!thePrompt) {
+    Facilitator.somethingWentWrong();
+    process.exit(1);
+  }
+
+  const cmd = await chatBot.ask(thePrompt);
+  const { choice } = await Facilitator.wantToTryIt(cmd);
+
+  if (choice === 'No') {
+    const { choice } = await Facilitator.askForAnotherCmd();
+
+    if (!choice) {
+      process.exit()
+    }
+
+    const { input } = await Facilitator.getMoreDetails();
+    if(input) {
+      await run(input);
+    }
+
+    await run(thePrompt)
+  }
+
+  if(choice === 'Copy to Clipboard') {
+     clipboard.writeSync(cmd);
+    process.exit();
+  }
+
+  try {
+    await execCmd(cmd);
+    Facilitator.success()
+    process.exit();
+  } catch (error) {
+    Facilitator.cmdFailed(error);
+    const { choice } = await Facilitator.tryAgain();
+    if (choice) {
+      await run(thePrompt);
+    }
+    process.exit();
+  }
+}
+
+program.version('1.0.0').action(async () => {
+  const { input } = await Facilitator.whatchaDoin();
+  await run(input);
+});
+
+program.parse();
